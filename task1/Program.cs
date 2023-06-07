6bf92dc554e7e30d29390fbf7723c1c2ce769e9d
ArhyParh <zescoarty@gmail.com>
2023-06-07 21:12:32 +0300

    Решил первые 2 задачи

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.Clear();
+Main();
+void Main()
+{
+    Console.WriteLine("Введите число N..");
+    int a = int.Parse(Console.ReadLine()!);
+    Console.WriteLine(GetNumberTill(a));
+
+}
+int GetNumberTill(int a)
+{ 
+    if(a==1){return 1;}
+    else 
+    Console.Write (a+", ");return GetNumberTill(a-1);
+}
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Main();
+void Main()
+{
+    Console.WriteLine("Введите число M...");
+    int M = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите число N...");
+    int N = int.Parse(Console.ReadLine()!);
+    int sum = ReturnSum(M, N);
+    Console.WriteLine(sum);
+}
+int ReturnSum(int a, int b)
+{
+    if (a == b) { return b; }
+    else return a += ReturnSum(a + 1, b);
+
+}
